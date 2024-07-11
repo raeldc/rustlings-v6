12e602068b3b8b524edade4e2240cbdcad88d432
Israel D. Canasa <rael@c-corp.dev>
2024-07-11 08:40:19 +0800

    Complete 08 enums

---
+++ exercises/08_enums/enums2.rs
@@ -0,0 +1,27 @@
+#[derive(Debug)]
+enum Message {
+    // TODO: Define the different variants used below.
+    Move { x: i32, y: i32 },
+    Echo(String),
+    ChangeColor(i16, i16, i16),
+    Quit,
+}
+
+impl Message {
+    fn call(&self) {
+        println!("{:?}", self);
+    }
+}
+
+fn main() {
+    let messages = [
+        Message::Move { x: 10, y: 30 },
+        Message::Echo(String::from("hello world")),
+        Message::ChangeColor(200, 255, 255),
+        Message::Quit,
+    ];
+
+    for message in &messages {
+        message.call();
+    }
+}
